73895d12976a94b2b2b74d74733c22ded13043b6
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-12-08 00:54:57 +0400

    add style to auth pages and 404

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 
 export default () => {
     return (
-      <p>NotFound component here</p>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Page Not Found</h1>
+          <p>Huumm, we're unable to find that page</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </div>
     );
 };
